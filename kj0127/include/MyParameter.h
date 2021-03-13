60358dd11f9f9206f0b9f8beb7faf18ed00c7f09
Sunsjtu <577572046@qq.com>
2021-03-13 17:49:27 +0800

    0313 commit

---
+++ kj0127/include/MyParameter.h
@@ -0,0 +1,21 @@
+﻿#ifndef MYPARAMETER_H
+#define MYPARAMETER_H
+
+#endif // MYPARAMETER_H
+
+#include "AdvMotApi.h"
+//#include "AdvMotDev.h"
+//#include "AdvMotDrv.h"
+//#include "AdvMotErr.h"
+//#include "AdvMotPropID.h"
+
+HAND pDevHandle;
+HAND pAxisHandle[32];//轴的Handle
+HAND pGroupHandle;//群组Handle
+U32 u32DevSerisNum;//设备编号
+U32 u32AxisCount;//轴数量
+U32 u32Return;//函数返回值
+
+
+I32 iComboXYZ; //控制调试部分XYZ轴切换相关
+bool bPosOrDistance; //绝对位置or相对位置
---
+++ kj0127/main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include"login.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    login dlg;
+    if(dlg.exec()==QDialog::Accepted)
+    {
+           w.show();
+           return a.exec();
+    }
+
+    else return 0;
+}
