d1512a4982c6ff7446321a7672fbc03853e394cb
hengliusjtu <577572046@qq.com>
2021-03-13 16:02:11 +0800

    0312 commit

---
+++ kj0127/login.h
@@ -0,0 +1,28 @@
+﻿#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QDialog>
+
+namespace Ui {
+class login;
+}
+
+class login : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit login(QWidget *parent = nullptr);
+    ~login();
+signals:
+    void showmain();
+private slots:
+    //void on_label_linkActivated(const QString &link);
+
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::login *ui;
+};
+
+#endif // LOGIN_H
